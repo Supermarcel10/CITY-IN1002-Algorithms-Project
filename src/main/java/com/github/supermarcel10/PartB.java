4c80b2be101128dcb1489462bce076d157287c58
Marcel <marcelbarlik@gmail.com>
2023-04-09 14:41:39 +0100

    - Made a working DPLL algorithm. 12/15

--- src/main/java/com/github/supermarcel10/PartB.java
+++ src/main/java/com/github/supermarcel10/PartB.java
@@ -12,14 +12,12 @@ public class PartB {
 		return result;
 	}
 
-	private static void debug(String message) {
-		System.out.println("DEBUG: " + message);
-	}
+//	private static void debug(String message) {
+//		System.out.println("DEBUG: " + message);
+//	}
 
 	private static int[] DPLL(int[][] clauses) {
-		Map<Integer, Integer> decisionLevels = new HashMap<>();
 		List<Map<Integer, Boolean>> decisionModels = new ArrayList<>();
-		int decisionLevel = 0;
 
 		List<List<Integer>> clauseList = new ArrayList<>();
 		for (int[] clause : clauses) {
@@ -38,13 +36,10 @@ public class PartB {
 		Map<Integer, Boolean> model = new HashMap<>();
 
 		stack.push(model);
-		debug("New Model: " + model);
 		decisionModels.add(new HashMap<>(model));
 
 		int[] assignment = new int[symbols.size() + 1]; // initialize assignment array
 		Arrays.fill(assignment, 0);
-
-		Map<Integer, List<Integer>> antecedentClauses = new HashMap<>();
 
 		while (!stack.empty()) {
 			model = stack.pop();
@@ -52,12 +47,11 @@ public class PartB {
 			// Unit Propagation
 			Map<Integer, Boolean> unitModel = new HashMap<>(model);
 			boolean unitPropagation = true;
-			Map<List<Integer>, Boolean> satisfiedCache = new HashMap<>();
 
 			while (unitPropagation) {
 				unitPropagation = false;
 				for (List<Integer> clause : clauseList) {
-					if (isClauseSatisfied(clause, unitModel, satisfiedCache)) {
+					if (isClauseSatisfied(clause, unitModel)) {
 						continue;
 					}
 
@@ -65,13 +59,11 @@ public class PartB {
 					if (unassignedLiterals.size() == 1) {
 						int unitLiteral = unassignedLiterals.get(0);
 						unitModel.put(Math.abs(unitLiteral), unitLiteral > 0);
-						antecedentClauses.put(Math.abs(unitLiteral), new ArrayList<>(clause));
 						unitPropagation = true;
 						break;
 					}
 				}
 			}
-			debug("After UP // Before PLE: " + unitModel);
 
 			model = unitModel;
 
@@ -94,12 +86,7 @@ public class PartB {
 				}
 			}
 
-			debug("After Pure Literal Elimination: " + pureModel);
-
-			if (pureLiteralFound) {
-				model = pureModel;
-				debug("Pure Literal Elimination: " + model);
-			}
+			if (pureLiteralFound) model = pureModel;
 
 			// Check if all clauses are satisfied
 			boolean allSatisfied = true;
@@ -122,47 +109,22 @@ public class PartB {
 				return assignment;
 			}
 
-			// Clause Learning
-			int conflict = -1;
-			for (List<Integer> clause : clauseList) {
-				if (isClauseSatisfied(clause, model)) {
-					continue;
-				}
+			// If there is an unassigned symbol, branch on its value
+			Map<Integer, Boolean> finalModel = model;
+			Optional<Integer> unassignedSymbol = symbols.stream().filter(s -> !finalModel.containsKey(s)).findFirst();
+			if (unassignedSymbol.isPresent()) {
+				int symbol = unassignedSymbol.get();
+				Map<Integer, Boolean> newModelTrue = new HashMap<>(model);
+				newModelTrue.put(symbol, true);
+				stack.push(newModelTrue);
 
-				conflict = clause.get(0);
-				break;
+				Map<Integer, Boolean> newModelFalse = new HashMap<>(model);
+				newModelFalse.put(symbol, false);
+				stack.push(newModelFalse);
+
+				decisionModels.add(new HashMap<>(newModelTrue));
+				decisionModels.add(new HashMap<>(newModelFalse));
 			}
-
-			int[] implicationGraph = createImplicationGraph(clauseList, model, decisionLevels, antecedentClauses);
-			int uip = findUIP(implicationGraph, conflict);
-			debug("Backtracking to decision level: " + uip);
-
-			if (uip == -1) {
-				return null; // UNSAT
-			}
-
-			while (decisionLevel > uip) {
-				if (stack.empty()) {
-					return null; // UNSAT
-				}
-				stack.pop();
-				decisionLevel--;
-			}
-			if (!stack.empty()) {
-				model = stack.peek();
-			}
-
-			if (decisionLevel + 1 < decisionModels.size()) {
-				decisionModels.subList(decisionLevel + 1, decisionModels.size()).clear();
-			}
-
-			stack.push(model);
-			decisionModels.add(new HashMap<>(model));
-			decisionLevel++;
-			debug("New Model: " + model);
-
-			decisionLevels.put(Math.abs(conflict), decisionLevel + 1);
-			model.put(Math.abs(conflict), conflict > 0);
 		}
 
 		return null; // UNSAT
@@ -175,59 +137,5 @@ public class PartB {
 			}
 		}
 		return false;
-	}
-
-	private static boolean isClauseSatisfied(List<Integer> clause, Map<Integer, Boolean> model, Map<List<Integer>, Boolean> cache) {
-		if (cache.containsKey(clause)) {
-			return cache.get(clause);
-		}
-		boolean satisfied = isClauseSatisfied(clause, model);
-		cache.put(clause, satisfied);
-		return satisfied;
-	}
-
-	private static int[] createImplicationGraph(List<List<Integer>> clauses, Map<Integer, Boolean> model, Map<Integer, Integer> decisionLevels, Map<Integer, List<Integer>> antecedentClauses) {
-		int maxSymbol = 0;
-		for (List<Integer> clause : clauses) {
-			for (int literal : clause) {
-				maxSymbol = Math.max(maxSymbol, Math.abs(literal));
-			}
-		}
-		int[] implicationGraph = new int[maxSymbol + 1];
-		Arrays.fill(implicationGraph, -1);
-
-		for (int literal : model.keySet()) {
-			if (decisionLevels.get(literal) == null || decisionLevels.get(literal) == 0) continue;
-			debug("Literal: " + literal);
-
-			List<Integer> antecedent = antecedentClauses.get(literal);
-			int otherAssigned = -1;
-
-			if (antecedent != null) {
-				for (int otherLiteral : antecedent) {
-					if (Math.abs(otherLiteral) != literal && model.get(Math.abs(otherLiteral)) == (otherLiteral > 0)) {
-						otherAssigned = Math.abs(otherLiteral);
-						break;
-					}
-				}
-			}
-			implicationGraph[literal] = otherAssigned;
-		}
-		return implicationGraph;
-	}
-
-
-	private static int findUIP(int[] implicationGraph, int conflict) {
-		int vertex = Math.abs(conflict);
-		int uip = -1;
-		while (uip == -1) {
-			int parent = implicationGraph[vertex];
-			if (parent == -1) {
-				uip = vertex;
-			} else {
-				vertex = parent;
-			}
-		}
-		return uip;
 	}
 }
